1b3fa350217b04d86304103d18f83a757ef1d1b6
mackdroid <68280146+mackdroid@users.noreply.github.com>
2022-03-18 15:37:18 +0530

    add randstuff cog

---
+++ cogs/randstuff.py
@@ -0,0 +1,15 @@
+from nextcord.ext import commands
+
+def setup(client):
+    client.add_cog(rand())
+    
+# Class for random stuff
+
+class rand(commands.Cog):
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot != False:
+            return
+        if message.content == "is gaeeeeeeeeeee":
+            await message.delete()
+            return
