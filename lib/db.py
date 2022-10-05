214129a0ef9e0de606640bddb52a2c5e4b9305a7
mackdroid <68280146+mackdroid@users.noreply.github.com>
2022-10-05 09:08:16 +0530

    add command choose

--- lib/db.py
+++ lib/db.py
@@ -1,32 +1,52 @@
 # simple json, key based database system, nothing fancy really
+from os import path
 import json
+from tkinter.messagebox import NO
 
 class DB():
     def __init__(self):
-        db = json.load(open("db.json").read)
+        if path.exists("db.json"):
+            self.db_file = open("db.json")
+        else:
+            self.db_file = open("db.json","x")
+            json.dump({},self.db_file)
+        self.db = json.loads(self.db_file.read())
         
     def write(self):
-        json.dump(self.db,open("db.json"))
+        json.dump(self.db,open("db.json","w"))
         
-    def upd_val(self,key,val):
+    def root_handle(self,root):
+        if root == None:
+            db = self.db
+        else:
+            root = root.split("/")
+            db = self.db
+            for i in root:
+                if i in db.keys():
+                    db = db[i]
+                else:
+                    raise IndexError
+        return db
+        
+    def upd_val(self,key,val,root=None):
+        db = self.root_handle(root)
         if type(val) == dict:
             raise TypeError
         else:
-            self.db[key]=val
+            db[key]=val
             self.write()
-
-    def del_val(self,key):
-        if key in self.db.keys():
+    
+    def del_val(self,key,root=None):
+        db = self.root_handle(root)
+        if key in db.keys():
             self.db[key]=None
             self.write()
         else:
             raise KeyError
         
-    def del_key(self,key):
+    def del_key(self,key,root=None):
+        db = self.root_handle(root)
         del self.db[key]
         self.write()
 
-    def create_subdb(self,sdb_name):
-        self.db[sdb_name]={}
-        self.write()
     
