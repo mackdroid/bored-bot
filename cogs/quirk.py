d68a6dbc22b01662bb4d9c9ce12c6968aa49ddd6
mackdroid <68280146+mackdroid@users.noreply.github.com>
2023-08-25 11:57:38 +0530

    add instagram fixes

---
+++ cogs/quirk.py
@@ -0,0 +1,18 @@
+from nextcord.ext import commands
+import re
+
+
+def setup(client):
+    client.add_cog(quirk())
+
+
+# Class for failry random stuff that are helpful
+
+class quirk(commands.Cog):
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if "https://instagram.com" in message.content:
+            webhook = await message.channel.create_webhook(name='hooker')
+            await webhook.send(content=re.sub(r'instagram\.com', 'ddinstagram.com', message.content),avatar_url = message.author.avatar.url,username=message.author.display_name)
+            await message.delete()
+            return
